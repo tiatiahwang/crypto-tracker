6122313e39ef035c86701bf8ae923d88a48d88b7
tiahwang44 <tiahwang44@gmail.com>
2022-03-24 20:03:16 +0900

    ADD: dark/light theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 import Router from './Router';
 import GlobalStyle from './styles/GlobalStyle';
+import { darkTheme, lightTheme } from './styles/theme';
 
 function App() {
+  // 추후 변경 예정
+  const [isDark, setIsDark] = useState(true);
   return (
-    <>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
       <GlobalStyle />
       <Router />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  bgColor: '#121212',
+  textColor: '#fcfcfc',
+  accentColor: '#F2CEDF',
+  cardColor: '#292929',
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: '#fcfcfc',
+  textColor: '#292929',
+  accentColor: '#F2CEDF',
+  cardColor: '#fff',
+};
